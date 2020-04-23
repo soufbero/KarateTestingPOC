cba74fd283a383aed92af72b24c6a444c8a437e6
Soufiane Berouel <soufiane.berouel@gmail.com>
2020-04-23 18:05:52 -0500

    Run specific apps based on jvm argument

--- AppsTesting/src/test/java/com/souf/karate/KarateIntegrationTest.java
+++ AppsTesting/src/test/java/com/souf/karate/KarateIntegrationTest.java
@@ -2,11 +2,20 @@ package com.souf.karate;
 
 import com.intuit.karate.junit5.Karate;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class KarateIntegrationTest {
 
     @Karate.Test
     Karate testFullPath() {
-        return Karate.run("classpath:karate");
+        List<String> paths = Arrays.stream(
+                System.getProperty("apps").split(","))
+                .map(c -> "classpath:karate/" + c).collect(Collectors.toList());
+        String[] pathsArray = new String[paths.size()];
+        paths.toArray(pathsArray);
+        return Karate.run(paths.toArray(pathsArray));
     }
 
 }
